ed2c84ad3a0b244101dc085a96625fcd115658fb
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-03-18 13:28:02 +0100

    refactor: format document

--- src/script.js
+++ src/script.js
@@ -1,34 +1,32 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('.webgl')
+const canvas = document.querySelector(".webgl");
 
 // Sizes
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: '#ff0000'})
-const mesh = new THREE.Mesh(geometry, material)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
+const mesh = new THREE.Mesh(geometry, material);
 mesh.position.x = 1;
 mesh.position.y = -0.6;
 mesh.position.z = 1;
-scene.add(mesh)
+scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
