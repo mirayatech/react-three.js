6133ba7799ff3ec6672663e1700b0de0f66d892c
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-03-19 09:52:00 +0100

    refactor: change code places

--- src/script.js
+++ src/script.js
@@ -3,14 +3,14 @@ import * as THREE from "three";
 // Canvas
 const canvas = document.querySelector(".webgl");
 
+// Scene
+const scene = new THREE.Scene();
+
 // Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-
-// Scene
-const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
